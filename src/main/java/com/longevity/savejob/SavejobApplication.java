8829ad8e8ac5e3f56c9e9ed43bf9a96bbbf87e3c
최수진 <tnwlssl0729@naver.com>
2022-08-03 17:56:28 +0900

    sj start

---
+++ src/main/java/com/longevity/savejob/SavejobApplication.java
@@ -0,0 +1,13 @@
+package com.longevity.savejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavejobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavejobApplication.class, args);
+	}
+}
+

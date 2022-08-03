272856cc8a0d539ffe48dc86ab231a5910811507
hyejeongjang <hj970219@naver.com>
2022-08-03 18:11:03 +0900

    hj branch test

--- src/main/java/com/longevity/savejob/SavejobApplication.java
+++ src/main/java/com/longevity/savejob/SavejobApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SavejobApplication {
 
+	// main
 	public static void main(String[] args) {
 		SpringApplication.run(SavejobApplication.class, args);
 	}

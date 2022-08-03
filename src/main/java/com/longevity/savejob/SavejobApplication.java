8f6c61ff061cc95ab43eb6492b8ed8210ac954c2
최수진 <tnwlssl0729@naver.com>
2022-08-03 23:24:08 +0900

    sj branch test

--- src/main/java/com/longevity/savejob/SavejobApplication.java
+++ src/main/java/com/longevity/savejob/SavejobApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SavejobApplication {
 
-	//main main
+	//main main main
 	public static void main(String[] args) {
 		SpringApplication.run(SavejobApplication.class, args);
 	}

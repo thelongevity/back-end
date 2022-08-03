2d7b78979381d48115c0201b74e2aff28f7c09b5
hyejeongjang <hj970219@naver.com>
2022-08-04 01:05:43 +0900

    hj branch, master final test

--- src/main/java/com/longevity/savejob/SavejobApplication.java
+++ src/main/java/com/longevity/savejob/SavejobApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SavejobApplication {
 
 	// main
-	// another main from hj
+	// push merge test by hj
 	public static void main(String[] args) {
 		SpringApplication.run(SavejobApplication.class, args);
 	}
